a7fa32f6b2dc2920f05e0c28cb6f5208a2a20d8f
abhishekBhure <abhiab7411@gmail.com>
2026-01-06 23:16:09 +0530

    cursor position tracker done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Canvas from './Learnings/CursorPosition/Canvas'
+
+function App() {
+
+  return (
+    <>
+      <Canvas />
+    </>
+  )
+}
+
+export default App
---
+++ src/Learnings/CursorPosition/Canvas.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { usePointerPosition } from './CustomHooks/usePointerPostion'
+import CursorDot from './CursorDot';
+
+const Canvas = () => {
+    const useDelayedValue = (value, delay) => {
+        const [delayedValue, setDelayedValue] = useState(value);
+
+        useEffect(() => {
+            setTimeout(() => {
+                setDelayedValue(value);
+            }, delay);
+        }, [value, delay]);
+        return delayedValue;
+    }
+
+    const pos1 = usePointerPosition();
+    const pos2 = useDelayedValue(pos1, 100);
+    const pos3 = useDelayedValue(pos2, 150);
+    const pos4 = useDelayedValue(pos3, 200);
+    const pos5 = useDelayedValue(pos4, 250);
+    const pos6 = useDelayedValue(pos5, 50);
+    return (
+        <>
+            <CursorDot position={pos1} opacity={1} />
+            <CursorDot position={pos2} opacity={0.8} />
+            <CursorDot position={pos3} opacity={0.6} />
+            <CursorDot position={pos4} opacity={0.4} />
+            <CursorDot position={pos5} opacity={0.2} />
+            <CursorDot position={pos6} opacity={0.15} />
+        </>
+    )
+}
+
+export default Canvas
